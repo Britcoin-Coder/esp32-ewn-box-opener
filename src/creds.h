1c8bf4aae630477b08a6d9d24a2bf5c982b51950
Britcoin-Coder <78276611+Britcoin-Coder@users.noreply.github.com>
2024-10-13 14:59:38 +0100

    Added creds.h for credentials

---
+++ src/creds.h
@@ -0,0 +1,10 @@
+//Moved creds to a separate file
+
+//=====wifi setup
+const char *ssid = ""; // <---------------------- SET THIS !!!
+const char *password = ""; // <-------------- SET THIS !!!
+
+//=====Box Opener client setup
+const char *apiUrl = "https://api.erwin.lol/"; // mainnet
+//const char *apiUrl = "https://devnet-api.erwin.lol/"; // devnet
+const char *apiKey = ""; // <---------------------- SET THIS !!!
